88c6eda7124aa793fa837e8bf2a59d25641baa67
MarianGhaly3 <marian.demian3@gmail.com>
2024-04-25 15:51:49 +0200

    route file update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Marian/{id}', function($id){
+    return 'Welcome to my first website ' . $id ;
+});
